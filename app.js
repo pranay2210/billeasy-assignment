5de336d799b905303740b27885390c2b2770dce5
Pranay Chandale <37976696+pranay2210@users.noreply.github.com>
2022-07-30 13:42:20 +0530

    employe db operations

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const department = require("./route/department");
 app.use(express.json());
 app.use("/employee", employee);
 app.use("/department", department);
-
+app.get('/',(req,res)=>{
+    res.send('Welcome to Bill Easy Assignment');
+});
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Listening on port ${process.env.PORT || 3000}`)
 );
